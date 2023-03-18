9571ee9590b31fbb0dc5aec235371ff3099f5d0d
SACHINHJIBHAKATE <sachinjibhakate@gmail.com>
2023-03-18 20:33:26 +0100

    Code to turn off uncaught exception handling

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -27,3 +27,9 @@ require('cypress-xpath')
 Cypress.Server.defaults({
     ignore: (xhr) => bool
   })
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
